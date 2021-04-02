2da5242578ac758d20b6672ac6c9276dfe3891f8
Diego Azevedo <dapptn@hotmail.com>
2021-04-03 01:29:24 +1100

    redirect to buttons as entry point

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Switch, Route } from "react-router";
+import { Switch, Route, Redirect } from "react-router";
 import Buttons from "../pages/Buttons";
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/">
-        <Buttons />
+        <Redirect to="/buttons" />
       </Route>
       <Route exact path="/buttons">
         <Buttons />
